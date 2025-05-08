2981d2268bd2d03f92a18049959848536f19fc04
Marcin <marcingrelak6@gmail.com>
2025-05-08 21:42:26 +0200

    updating scraper after reading more into docu

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ def create_folder(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def get_dir_size(path):
+#ommitting folders since crawler will only create files
+def get_dir_size(dir):
     total_size = 0;
-    for filenames in os.walk(path):
+    for dirpath, dirnames, filenames in os.walk(dir):
         for f in filenames:
-            total_size += os.path.getsize(os.path.join(dirpath, f))
-            if not os.path.islink(os.path.join(dirpath, f)):
-                total_size += os.path.getsize(os.path.join(dirpath, f))
+            file_path = os.path.join(dirpath, f)
+            if os.path.isfile(file_path):
+                total_size += os.path.getsize(file_path)
     return total_size
 
 def download_page(title, lang, dir):
-    wiki = wikipediaapi.Wikipedia(lang, user_agent="WikiScraper/0.1 (marcingrelak6@gmail.com) wikipediaapi/0.8.1")
+    wiki = wikipediaapi.Wikipedia(
+    user_agent="WikiScraperBot/0.1 (marcingrelak6@gmail.com) wikipediaapi/0.8.1",
+    language='en',
+    extract_format=wikipediaapi.ExtractFormat.WIKI
+    )
+
     page = wiki.page(title)
     if page.exists():
         path = os.path.join(dir, f"{page.title}_{lang}.txt")
         with open(path, "wb", encoding='utf-8') as file:
-            file.write(page.content)
+            file.write(page.text.encode('utf-8'))
         print("downloaded" f"{page.title}_{lang}")
     else:
         print("no page found")
