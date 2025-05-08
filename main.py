960f284c00cf0367e75d6291b018e2e6b6e164e5
Marcin <marcingrelak6@gmail.com>
2025-05-08 15:46:16 +0200

    start of python crawler

---
+++ main.py
@@ -0,0 +1,27 @@
+import wikipediaapi
+import os
+from urllib.parse import urljoin
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def get_dir_size(path):
+    total_size = 0;
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            total_size += os.path.getsize(os.path.join(dirpath, f))
+            if not os.path.islink(os.path.join(dirpath, f)):
+                total_size += os.path.getsize(os.path.join(dirpath, f))
+    return total_size
+
+def download_page(title, lang, dir):
+    wiki = wikipediaapi.Wikipedia(lang)
+    page = wiki.page(title)
+
+    if page.exists():
+        path = os.path.join(dir, f"{page.title}_{lang}.txt")
+        with open(path, "wb", encoding='utf-8') as file:
+            file.write(page.content)
+        print("downloaded" f"{page.title}_{lang}")
+    else:
+        print("no page found")
