0d57a827ee61a545bdebc8ff912fcb494faf743d
Marcin <marcingrelak6@gmail.com>
2025-09-11 21:18:03 +0200

    added read timeout 30 sec and modifiet connection timeout to 15 sec

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def get_random_page_title(wiki):
     user_agent = "WikiScraperBot/0.5 (marcingrelak6@gmail.com;) wikipediaapi/0.8.1"
 #    print("Getting random page title...")
     try:
-        response = requests.get(f'https://{wiki.language}.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json', headers={'User-Agent': user_agent}, timeout=30)
+        response = requests.get(f'https://{wiki.language}.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json', headers={'User-Agent': user_agent}, timeout=(15,30))
     except requests.exceptions.ReadTimeout:
         print("read timeout error")
         return "Odessa Brigade"
