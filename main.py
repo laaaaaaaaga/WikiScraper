b95ee53b20f9e343d9370f2af3985884f6f082db
Marcin <marcingrelak6@gmail.com>
2025-09-11 23:03:02 +0200

    replaced both agents with global variable, updated version to v0.9

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import wikipediaapi
 import os
 import requests
 
+AGENT = "WikiScraperBot/0.9 (marcingrelak6@gmail.com;) wikipediaapi/0.8.1"
 MAX_FOLDER_SIZE = 8*1024*1024*3 # in MB
 language_codes = ["en", "es", "fr", "de", "zh", "pt", "ru", "ja", "ko"]
 more_language_codes = ["af", "als", "am", "an", "ar", "arc", "ary", "as",
@@ -51,14 +52,14 @@ def get_dir_size(directory):
 def init_wiki(lang):
     print("Initializing Wikipedia...")
     return wikipediaapi.Wikipedia(
-    user_agent="WikiScraperBot/0.7 (marcingrelak6@gmail.com;) wikipediaapi/0.8.1",
+    user_agent=AGENT,
     language=lang,
     timeout = (15, None),
     extract_format=wikipediaapi.ExtractFormat.WIKI
     )
 
 def get_random_page_title(wiki):
-    user_agent = "WikiScraperBot/0.7 (marcingrelak6@gmail.com;) wikipediaapi/0.8.1"
+    user_agent = AGENT
 #    print("Getting random page title...")
     try:
         response = requests.get(f'https://{wiki.language}.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json', headers={'User-Agent': user_agent}, timeout=(15, None))
